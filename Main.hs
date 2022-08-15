b79058fbdfd8f56a2df5063dbea38c08ceabbe4f
Akihisa Yagi <caphosra@gmail.com>
2022-08-15 18:04:57 +0900

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,18 +1,31 @@
 import Lexer
 import Parser
-
-parse :: String -> IO ()
-parse cal =
-    case tokenize cal of
-        Valid tokens ->
-            do
-                putStrLn "-----"
-                putStrLn ("Tokens   : " ++ show tokens)
-                putStrLn ("Statement: " ++ show (parseStatement tokens))
-        Error err -> print err
+import Operation
 
 main =
     do
-        parse "exp = lambda n. lambda m. m n"
-        parse "succ = lambda n. lambda s. lambda z. n s (s z)"
-        parse "lambda x. x lambda x. x (x x)"
+        case tokenize "(lambda n. lambda m. lambda s. lambda z. n s (m s z)) (lambda s. lambda z. s z) (lambda s. lambda z. s z)" of
+            Valid tokens ->
+                do
+                    case parseLambdaCal tokens of
+                        Valid cal ->
+                            do
+                                putStrLn "---- the normal order strategy ----"
+                                printProcesses betaNO cal []
+                                putStrLn "---- the call by name strategy ----"
+                                printProcesses betaCN cal []
+                                putStrLn "---- the call by value strategy ----"
+                                printProcesses betaCV cal []
+                        Error err -> print err
+            Error err -> print err
+
+printProcesses :: (LambdaCal -> LambdaCal) -> LambdaCal -> [LambdaCal] -> IO ()
+printProcesses beta cal appeared =
+    do
+        putStrLn (showLambdaCal cal)
+        if cal `elem` appeared then
+            pure ()
+        else
+            do
+                putStr "→ "
+                printProcesses beta (beta cal) (cal : appeared)
