81260e93d870a885bbd16933348656d3f47b5891
Akihisa Yagi <caphosra@gmail.com>
2022-08-19 18:11:32 +0900

    Reimplement beta-reduction

---
+++ src/BetaReduction.hs
@@ -0,0 +1,66 @@
+module BetaReduction (
+    ReductionResult (
+        Reduced,
+        NormalForm
+    ),
+    ReductionStrategy (
+        NormalOrder,
+        CallByName,
+        CallByValue
+    ),
+    beta,
+) where
+
+import LambdaCalculus
+import Operation
+
+-- Holds a result of beta-reduction.
+data ReductionResult
+    = Reduced LambdaCal
+    | NormalForm LambdaCal
+
+-- A strategy conducting beta-reduction repeatedly.
+data ReductionStrategy
+    = NormalOrder
+    | CallByName
+    | CallByValue
+
+-- Applies a function to the term of the result.
+mapResult :: ReductionResult -> (LambdaCal -> LambdaCal) -> ReductionResult
+mapResult (Reduced term) f = Reduced (f term)
+mapResult (NormalForm term) f = NormalForm (f term)
+
+-- Conducts beta-reduction.
+beta :: ReductionStrategy -> LambdaCal -> ReductionResult
+beta NormalOrder = betaNO
+beta CallByName = betaCN
+beta CallByValue = betaCV
+
+-- Conducts beta-reduction with the normal form strategy.
+betaNO :: LambdaCal -> ReductionResult
+betaNO (Abst name child) = mapResult (betaNO child) (Abst name)
+betaNO (App (Abst name child) replaceTo) = Reduced (replaceVariable name replaceTo child)
+betaNO (App left right) =
+    case betaNO left of
+        Reduced red -> Reduced (App red right)
+        NormalForm _ -> mapResult (betaNO right) (App left)
+betaNO cal = NormalForm cal
+
+-- Conducts beta-reduction with the call by name strategy.
+betaCN :: LambdaCal -> ReductionResult
+betaCN (App (Abst name child) replaceTo) = Reduced (replaceVariable name replaceTo child)
+betaCN (App left right) =
+    case betaCN left of
+        Reduced red -> Reduced (App red right)
+        NormalForm _ -> mapResult (betaCN right) (App left)
+betaCN cal = NormalForm cal
+
+-- Conducts beta-reduction with the call by value strategy.
+betaCV :: LambdaCal -> ReductionResult
+betaCV (App (Abst name1 child1) (Abst name2 child2)) =
+    Reduced (replaceVariable name1 (Abst name2 child2) child1)
+betaCV (App left right) =
+    case betaCV left of
+        Reduced red -> Reduced (App red right)
+        NormalForm _ -> mapResult (betaCV right) (App left)
+betaCV cal = NormalForm cal
--- src/Operation.hs
+++ src/Operation.hs
@@ -1,9 +1,6 @@
 module Operation (
     replaceVariable,
     replaceAll,
-    betaNO,
-    betaCN,
-    betaCV,
 ) where
 
 import Data.Bifunctor (Bifunctor(second))
@@ -40,29 +37,3 @@ replaceAll vars cal =
     removeMark
     $ foldl (flip (uncurry replaceVariable)) cal
     $ map (second prependMark) vars
-
--- Conducts beta-reduction with the normal form strategy.
-betaNO :: LambdaCal -> LambdaCal
-betaNO (Abst name child) = Abst name (betaNO child)
-betaNO (App (Abst name child) replaceTo) = replaceVariable name replaceTo child
-betaNO (App left right)
-    | betaNO left /= left = App (betaNO left) right
-    | otherwise = App left (betaNO right)
-betaNO cal = cal
-
--- Conducts beta-reduction with the call by name strategy.
-betaCN :: LambdaCal -> LambdaCal
-betaCN (App (Abst name child) replaceTo) = replaceVariable name replaceTo child
-betaCN (App left right)
-    | betaCN left /= left = App (betaCN left) right
-    | otherwise = App left (betaCN right)
-betaCN cal = cal
-
--- Conducts beta-reduction with the call by value strategy.
-betaCV :: LambdaCal -> LambdaCal
-betaCV (App (Abst name1 child1) (Abst name2 child2)) =
-    replaceVariable name1 (Abst name2 child2) child1
-betaCV (App left right)
-    | betaCV left /= left = App (betaCV left) right
-    | otherwise = App left (betaCV right)
-betaCV cal = cal
