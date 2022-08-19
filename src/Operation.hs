b40d3f4a1a97363e96233a4c024af99d1c1b1d53
Akihisa Yagi <caphosra@gmail.com>
2022-08-19 14:59:02 +0900

    Implement replaceAll

--- src/Operation.hs
+++ src/Operation.hs
@@ -1,9 +1,12 @@
 module Operation (
     replaceVariable,
+    replaceAll,
     betaNO,
     betaCN,
     betaCV,
 ) where
+
+import Data.Bifunctor (Bifunctor(second))
 
 import LambdaCalculus
 
@@ -17,6 +20,26 @@ replaceVariable v replaceTo (Abst name cal)
     | otherwise = Abst name (replaceVariable v replaceTo cal)
 replaceVariable v replaceTo (App left right) =
     App (replaceVariable v replaceTo left) (replaceVariable v replaceTo right)
+
+-- Prepends "!" marks to all of the variables in the term.
+prependMark :: LambdaCal -> LambdaCal
+prependMark (Variable name) = Variable ("!" ++ name)
+prependMark (Abst name child) = Abst name (prependMark child)
+prependMark (App child1 child2) = App (prependMark child1) (prependMark child2)
+
+-- Removes "!" marks from the variables.
+removeMark :: LambdaCal -> LambdaCal
+removeMark (Variable ('!' : rest)) = Variable rest
+removeMark (Variable name) = Variable name
+removeMark (Abst name child) = Abst name (removeMark child)
+removeMark (App child1 child2) = App (removeMark child1) (removeMark child2)
+
+-- Replaces the variables. Don't confuse with `replaceVariable`.
+replaceAll :: [(String, LambdaCal)] -> LambdaCal -> LambdaCal
+replaceAll vars cal =
+    removeMark
+    $ foldl (flip (uncurry replaceVariable)) cal
+    $ map (second prependMark) vars
 
 -- Conducts beta-reduction with the normal form strategy.
 betaNO :: LambdaCal -> LambdaCal
