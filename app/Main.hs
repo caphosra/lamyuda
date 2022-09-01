1069f3fcf57b005de99d1b8597da5b935bc9ae7f
Akihisa Yagi <caphosra@gmail.com>
2022-09-01 02:53:48 +0000

    Loosen coupling around the configuration

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 module Main (main) where
 
-import System.IO
 import Control.Monad.IO.Class
-import System.Console.Haskeline hiding (outputStrLn)
+import System.Console.Haskeline
+import System.IO
 
 import BetaReduction
+import Configuration
 import Evaluator
 import LambdaCalculus
 import Lexer
@@ -16,84 +17,87 @@ main = do
     hSetEncoding stdin utf8
     hSetEncoding stdout utf8
     putStrLn "Lamdba -- a simple lambda calculus operator"
-    runInputT defaultSettings (promptLoop (NormalOrder, []))
-
-type Config = (ReductionStrategy, [(String, LambdaCal)])
+    runInputT defaultSettings (promptLoop defaultConfig)
 
 promptLoop :: Config -> InputT IO ()
 promptLoop config = do
     rawInput <- getInputLine "> "
     case rawInput of
-        Just input ->
-            tokenizerProc input
+        Just input -> do
+            result <- liftIO
+                $ tokenizerProc input
                 $ parserProc
-                $ evalProc promptLoop config
+                $ evalProc config
+            case result of
+                KeepAlive diff ->
+                    promptLoop $ applyDiff diff config
+                Quit -> return ()
         Nothing -> return ()
 
-outputStrLn :: String -> InputT IO ()
-outputStrLn s = do liftIO $ putStrLn s
-
-tokenizerProc :: String -> ([(Int, Token)] -> InputT IO ()) -> InputT IO ()
+tokenizerProc :: String -> ([(Int, Token)] -> IO PromptResult) -> IO PromptResult
 tokenizerProc input postProc = do
     case tokenize input of
         Valid tokens -> postProc tokens
-        Error (pos, c) ->
-            outputStrLn ("Lexer error: An invalid character \"" ++ c ++ "\" was found at " ++ show pos)
+        Error (pos, c) -> do
+            putStrLn ("Lexer error: An invalid character \"" ++ c ++ "\" was found at " ++ show pos)
+            return $ KeepAlive unmodified
 
-parserProc :: (Statement -> InputT IO ()) -> [(Int, Token)] -> InputT IO ()
+parserProc :: (Statement -> IO PromptResult) -> [(Int, Token)] -> IO PromptResult
 parserProc postProc tokens = do
     case parseStatement tokens of
         Valid st -> postProc st
-        Error (pos, c) ->
-            outputStrLn ("Syntax error: An unexpected token \"" ++ toStr c ++ "\" was found at " ++ show pos)
+        Error (pos, c) -> do
+            putStrLn ("Syntax error: An unexpected token \"" ++ toStr c ++ "\" was found at " ++ show pos)
+            return $ KeepAlive unmodified
 
-evalProc :: (Config -> InputT IO ()) -> Config -> Statement -> InputT IO ()
-evalProc exec (strategy, functions) (FuncDef name term)
+evalProc :: Config -> Statement -> IO PromptResult
+evalProc (_, functions) (FuncDef name term)
     | any ((name ==) . fst) functions = do
-        outputStrLn ("\"" ++ name ++ "\" was already defined.")
-        outputStrLn ("Previous : " ++ name ++ " = " ++ showLambdaCal prevTerm)
-        outputStrLn ("Redefined: " ++ name ++ " = " ++ showLambdaCal term)
-        exec (strategy, updated)
+        putStrLn ("\"" ++ name ++ "\" was already defined.")
+        putStrLn ("Previous : " ++ name ++ " = " ++ showLambdaCal prevTerm)
+        putStrLn ("Redefined: " ++ name ++ " = " ++ showLambdaCal term)
+        return $ KeepAlive (Unmodified, Modified updated)
     | otherwise = do
-        outputStrLn ("Defined: " ++ name ++ " = " ++ showLambdaCal term)
-        exec (strategy, updated)
+        putStrLn ("Defined: " ++ name ++ " = " ++ showLambdaCal term)
+        return $ KeepAlive (Unmodified, Modified updated)
     where
         prevTerm = snd $ head $ filter ((name ==) . fst) functions
         updated = (name, term) : filter ((name /=) . fst) functions
-evalProc exec (strategy, functions) (Eval cal) = do
-    outputStrLn (showLambdaCal cal)
+evalProc (strategy, functions) (Eval cal) = do
+    putStrLn (showLambdaCal cal)
     replaced <- liftIO $ replaceFunction 5 cal functions
     liftIO $ betaReduction 30 (beta strategy) replaced [replaced]
-    exec (strategy, functions)
-evalProc exec (strategy, functions) (Exec ["list"]) = do
+    return $ KeepAlive unmodified
+evalProc (_, functions) (Exec ["list"]) = do
     printFunctionsList functions
-    exec (strategy, functions)
+    return $ KeepAlive unmodified
     where
-        printFunctionsList :: [(String, LambdaCal)] -> InputT IO ()
+        printFunctionsList :: [(String, LambdaCal)] -> IO ()
         printFunctionsList [] = return ()
         printFunctionsList ((name, func) : rest) = do
-            outputStrLn (name ++ " = " ++ showLambdaCal func)
+            putStrLn (name ++ " = " ++ showLambdaCal func)
             printFunctionsList rest
-evalProc exec (NormalOrder, functions) (Exec ["strategy"]) = do
-    outputStrLn "Current strategy : Normal Order"
-    exec (NormalOrder, functions)
-evalProc exec (CallByName, functions) (Exec ["strategy"]) = do
-    outputStrLn "Current strategy : Call by Name"
-    exec (CallByName, functions)
-evalProc exec (CallByValue, functions) (Exec ["strategy"]) = do
-    outputStrLn "Current strategy : Call by Value"
-    exec (CallByValue, functions)
-evalProc exec (_, functions) (Exec ["strategy", "no"]) = do
-    outputStrLn "Strategy : Normal Order"
-    exec (NormalOrder, functions)
-evalProc exec (_, functions) (Exec ["strategy", "cn"]) = do
-    outputStrLn "Strategy : Call by Name"
-    exec (CallByName, functions)
-evalProc exec (_, functions) (Exec ["strategy", "cv"]) = do
-    outputStrLn "Strategy : Call by Value"
-    exec (CallByValue, functions)
-evalProc _ _ (Exec ["exit"]) = do
-    outputStrLn "Quit."
-evalProc exec config _ = do
-    outputStrLn "Invalid command."
-    exec config
+evalProc (NormalOrder, _) (Exec ["strategy"]) = do
+    putStrLn "Current strategy : Normal Order"
+    return $ KeepAlive unmodified
+evalProc (CallByName, _) (Exec ["strategy"]) = do
+    putStrLn "Current strategy : Call by Name"
+    return $ KeepAlive unmodified
+evalProc (CallByValue, _) (Exec ["strategy"]) = do
+    putStrLn "Current strategy : Call by Value"
+    return $ KeepAlive unmodified
+evalProc (_, _) (Exec ["strategy", "no"]) = do
+    putStrLn "Strategy : Normal Order"
+    return $ KeepAlive (Modified NormalOrder, Unmodified)
+evalProc (_, _) (Exec ["strategy", "cn"]) = do
+    putStrLn "Strategy : Call by Name"
+    return $ KeepAlive (Modified CallByName, Unmodified)
+evalProc (_, _) (Exec ["strategy", "cv"]) = do
+    putStrLn "Strategy : Call by Value"
+    return $ KeepAlive (Modified CallByValue, Unmodified)
+evalProc _ (Exec ["exit"]) = do
+    putStrLn "Quit."
+    return Quit
+evalProc _ _ = do
+    putStrLn "Invalid command."
+    return $ KeepAlive unmodified
