29a8f96b025c847d5fde54ee2ee7f06a900d5c85
Akihisa Yagi <caphosra@gmail.com>
2022-10-08 08:53:49 +0000

    Refactor Configuration.hs

--- app/Configuration.hs
+++ app/Configuration.hs
@@ -20,35 +20,54 @@ import Data.Tuple.All
 import BetaReduction
 import LambdaCalculus
 
+--
 -- Represents a difference if exists.
+--
 data Diff a
     = Modified a
     | Unmodified
 
+--
 -- The result of the prompt.
+--
 data PromptResult
     = KeepAlive ConfigMod
     | Quit
 
+--
 -- Represents a context Γ.
+--
 type Context = [(String, LambdaCal)]
 
+--
 -- A configuration of the prompt.
+--
 type Config = (ReductionStrategy, Context)
 
+--
 -- A modification of the configuration.
+--
 type ConfigMod = (Diff ReductionStrategy, Diff Context)
 
+--
 -- Returns a modification that holds "no difference".
+--
 unmodified :: ConfigMod
+
 unmodified = (Unmodified, Unmodified)
 
+--
 -- The default configuration.
+--
 defaultConfig :: Config
+
 defaultConfig = (NormalOrder, [])
 
+--
 -- Applies a modification to the configuration used.
+--
 applyDiff :: ConfigMod -> Config -> Config
+
 applyDiff modification =
     case modification of
         (Modified strategy, _) ->
